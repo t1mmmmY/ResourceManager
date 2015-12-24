38629a485fe2d7855e2e67ba8d35fcf21542b0d4
denis <dovganichd@digicode.net>
2015-12-24 17:42:18 +0100

    Properties like popup list

---
+++ Assets/EditorScript/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ComponentProperties  
+{
+	public string[] properties = {"position", "color", "anything"};
+	public int current = 0;
+}
+
+public class SomeClass : MonoBehaviour 
+{
+	[SerializeField] ComponentProperties potionResult;
+	public string otherStuff;
+}
