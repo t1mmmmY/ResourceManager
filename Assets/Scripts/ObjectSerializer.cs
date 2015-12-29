6ce873eac64334e32f4a5678cf1152cb4610dcbc
Konstantyn Ishchenko <ishchenkok@digicode.net>
2015-12-29 11:56:40 +0200

    Property serialized

--- Assets/Scripts/ObjectSerializer.cs
+++ Assets/Scripts/ObjectSerializer.cs
@@ -19,6 +19,11 @@ public class Parameter
 	public string ParamName;
 	public string ParamType;
 	public string Value;
+
+	public override string ToString ()
+	{
+		return string.Format ("<{0}>.{1} ({2}) = '{3}'", ComponentName, ParamName, ParamType, Value);
+	}
 }
 
 [Serializable]
@@ -97,12 +102,32 @@ public class ObjectSerializer : MonoBehaviour
 				Debug.Log("Field: " + f.Name + " : " + f.FieldType + "; " + f.GetValue(component));
 			}
 
-			var ignoredTypes = new List<Type>();
-			ignoredTypes.Add(typeof(UnityEngine.Rigidbody));
+			var ignoredProperties = new List<String>();
+			ignoredProperties.Add("rigidbody");
+			ignoredProperties.Add("rigidbody2D");
+			ignoredProperties.Add("camera");
+			ignoredProperties.Add("light");
+			ignoredProperties.Add("animation");
+			ignoredProperties.Add("constantForce");
+			ignoredProperties.Add("renderer");
+			ignoredProperties.Add("audio");
+			ignoredProperties.Add("guiText");
+			ignoredProperties.Add("networkView");
+			ignoredProperties.Add("guiElement");
+			ignoredProperties.Add("guiTexture");
+			ignoredProperties.Add("collider");
+			ignoredProperties.Add("collider2D");
+			ignoredProperties.Add("hingeJoint");
+			ignoredProperties.Add("particleEmitter");
+			ignoredProperties.Add("particleSystem");
 
-			foreach (var p in type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(i => !ignoredTypes.Contains(i.PropertyType)))
+			// Ignore this two for now
+			ignoredProperties.Add("mesh");
+			ignoredProperties.Add("material");
+
+			foreach (var p in type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(i => !ignoredProperties.Contains(i.Name)))
 			{
-				Debug.Log("Property: " + p.Name + " : " + p.PropertyType);// + "; " + p.GetValue(component, null));
+				Debug.Log("Property: '" + p.Name + "' [" + p.PropertyType + "] Value: " + p.GetValue(component, null));
 			}
 		}
 	}
@@ -138,12 +163,25 @@ public class ObjectSerializer : MonoBehaviour
 				// Simple types derived from struct and string serialize as is
 				if (f.FieldType.IsValueType || f.FieldType == typeof(String))
 				{
+					string paramValue = string.Empty;
+					if (f.FieldType == typeof(Vector3)) 
+					{
+						paramValue = UnityTypesConverter.Vector3ToString(((Vector3)f.GetValue(component)));
+					}
+					else if (f.FieldType == typeof(Quaternion))
+					{
+						paramValue = UnityTypesConverter.QuaternionToString(((Quaternion)f.GetValue(component)));
+					}
+					else
+					{
+						paramValue = f.GetValue(component).ToString();
+					}
+
 					Parameter param = new Parameter();
 					param.ComponentName = type.ToString();
 					param.ParamName = f.Name;
 					param.ParamType = f.FieldType.ToString();
-					param.Value = f.GetValue(component).ToString();
-						
+					param.Value = paramValue;	
 					modeParams.Parameters.Add(param);
 				}
 				// UnityEngine objects save in cachedUnityObjects list which is serialized by Unity in UnityEditor
@@ -154,7 +192,7 @@ public class ObjectSerializer : MonoBehaviour
 					param.ParamName = f.Name;
 					param.ParamType = "CachedUnityObject";
 					param.Value = type + "." + f.Name;
-
+					
 					// Remove old parameter for this mode
 					this.cachedUnityObjects.RemoveAll(i => i.Key == param.Value && i.ModeGUID == modeGUID);
 					// Add new parameter for this mode
@@ -165,11 +203,52 @@ public class ObjectSerializer : MonoBehaviour
 				}
 			}
 			//TODO Implment processing Properties
-//			BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
-//			foreach (var p in type.GetProperties(flags).Where(i => parameterList.Contains(i.Name)))
-//			{
-//				Debug.Log("Property: " + p.Name + " : " + p.PropertyType + "; " + p.GetValue(component, null));
-//			}
+			BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
+			foreach (var p in type.GetProperties(flags).Where(i => parameterList.Contains(i.Name)))
+			{
+				// Simple types derived from struct and string serialize as is
+				if (p.PropertyType.IsValueType || p.PropertyType == typeof(String))
+				{
+					string paramValue = string.Empty;
+					if (p.PropertyType == typeof(Vector3)) 
+					{
+						paramValue = UnityTypesConverter.Vector3ToString(((Vector3)p.GetValue(component, null)));
+					}
+					else if (p.PropertyType == typeof(Quaternion))
+					{
+						paramValue = UnityTypesConverter.QuaternionToString(((Quaternion)p.GetValue(component, null)));
+					}
+					else
+					{
+						paramValue = p.GetValue(component, null).ToString();
+					}
+
+					Parameter param = new Parameter();
+					param.ComponentName = type.ToString();
+					param.ParamName = p.Name;
+					param.ParamType = p.PropertyType.ToString();
+					param.Value = paramValue;
+
+					modeParams.Parameters.Add(param);
+				}
+				// UnityEngine objects save in cachedUnityObjects list which is serialized by Unity in UnityEditor
+				if (p.PropertyType.IsSubclassOf(typeof(UnityEngine.Object)))
+				{
+					Parameter param = new Parameter();
+					param.ComponentName = type.ToString();
+					param.ParamName = p.Name;
+					param.ParamType = "CachedUnityObject";
+					param.Value = type + "." + p.Name;
+
+					// Remove old parameter for this mode
+					this.cachedUnityObjects.RemoveAll(i => i.Key == param.Value && i.ModeGUID == modeGUID);
+					// Add new parameter for this mode
+					this.cachedUnityObjects.Add(new CachedParameter(param.Value, 
+						modeGUID,
+						(UnityEngine.Object)p.GetValue(component, null)));
+					modeParams.Parameters.Add(param);
+				}			
+			}
 		}
 
 		return JsonUtility.ToJson(modeParams, false);
@@ -180,19 +259,20 @@ public class ObjectSerializer : MonoBehaviour
 		// Parse JSON back to objects
 		var modeParams = JsonUtility.FromJson<ModeParameters>(serializedMode);
 		// Build list that contains Components (only necessary information)
-		var trackableComponentsList = modeParams.Parameters.Select(i => Type.GetType(i.ComponentName));
+		var trackableComponentsList = modeParams.Parameters.Select(i => i.ComponentName);
 		// Build list of gameObject's components that present in prev list
-		var trackableComponents = gameObject.GetComponents(typeof(Component)).Where(c => trackableComponentsList.Contains(c.GetType()));
+		var trackableComponents = gameObject.GetComponents(typeof(Component)).Where(c => trackableComponentsList.Contains(c.GetType().ToString()));
 		// Go through particular gameObject's components only
 		foreach (var component in trackableComponents)
 		{
 			// Get type of component
 			Type type = component.GetType();
+
 			// Go through each field in this component
 			foreach (var f in type.GetFields().Where(i => i.IsPublic))
 			{
 				// Find if there is a record for this field in ModeParameters
-				var prm = modeParams.Parameters.FirstOrDefault(i => Type.GetType(i.ComponentName) == type && i.ParamName == f.Name);
+				var prm = modeParams.Parameters.FirstOrDefault(i => i.ComponentName == type.ToString() && i.ParamName == f.Name);
 				// If record was found
 				if (prm != default(Parameter))
 				{
@@ -207,7 +287,18 @@ public class ObjectSerializer : MonoBehaviour
 						}
 						else
 						{
-							obj = Convert.ChangeType(prm.Value, f.FieldType);
+							if (f.FieldType == typeof(Vector3))
+							{
+								obj = UnityTypesConverter.Vector3FromString(prm.Value);
+							}
+							else if (f.FieldType == typeof(Quaternion))
+							{
+								obj = UnityTypesConverter.QuaternionFromString(prm.Value);
+							}
+							else
+							{
+								obj = Convert.ChangeType(prm.Value, f.FieldType);
+							}
 						}
 						f.SetValue(component, obj);
 					}
@@ -219,6 +310,55 @@ public class ObjectSerializer : MonoBehaviour
 						if (cachedObj != default(CachedParameter))
 						{
 							f.SetValue(component, cachedObj.Value);
+						}
+					}
+				}
+			}
+
+			// Go through each property in this component
+			BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
+			foreach (var p in type.GetProperties(flags))
+			{
+				// Find if there is a record for this field in ModeParameters
+				var prm = modeParams.Parameters.FirstOrDefault(i => i.ComponentName == type.ToString() && i.ParamName == p.Name);
+				// If record was found
+				if (prm != default(Parameter))
+				{
+					// If this is simple type then restore value
+					if (prm.ParamType != "CachedUnityObject")
+					{
+						object obj;
+
+						if (p.PropertyType.IsEnum)
+						{
+							obj = Enum.Parse(p.PropertyType, prm.Value);
+						}
+						else
+						{
+							if (p.PropertyType == typeof(Vector3))
+							{
+								obj = UnityTypesConverter.Vector3FromString(prm.Value);
+							}
+							else if (p.PropertyType == typeof(Quaternion))
+							{
+								obj = UnityTypesConverter.QuaternionFromString(prm.Value);
+							}
+							else
+							{
+								obj = Convert.ChangeType(prm.Value, p.PropertyType);
+							}
+						}
+						p.SetValue(component, obj, null);
+					}
+					// Othervise take object from cache
+					else
+					{
+						var cachedObj = this.cachedUnityObjects.FirstOrDefault(i => i.Key == prm.Value && i.ModeGUID == modeParams.GUID);
+
+						if (cachedObj != default(CachedParameter))
+						{
+							Debug.Log(cachedObj.Value);
+							p.SetValue(component, cachedObj.Value, null);
 						}
 					}
 				}
---
+++ Assets/Scripts/UnityTypesConverter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+class UnityTypesConverter
+{
+	// Vector3 section
+	public static string Vector3ToString(Vector3 v)
+	{
+		return string.Format("{0:0.00000},{1:0.00000},{2:0.00000}", v.x, v.y, v.z);
+	}
+
+	public static Vector3 Vector3FromString(String s)
+	{
+		string[] parts = s.Split(new string[] { "," }, StringSplitOptions.None);
+		return new Vector3(
+			float.Parse(parts[0]),
+			float.Parse(parts[1]),
+			float.Parse(parts[2]));
+	}
+
+	// Quaternion section
+	public static string QuaternionToString(Quaternion q)
+	{
+		return string.Format("{0:0.00000},{1:0.00000},{2:0.00000},{3:0.00000}", q.x, q.y, q.z, q.w);
+	}
+
+	public static Quaternion QuaternionFromString(String s)
+	{
+		string[] parts = s.Split(new string[] { "," }, StringSplitOptions.None);
+		return new Quaternion(
+			float.Parse(parts[0]),
+			float.Parse(parts[1]),
+			float.Parse(parts[2]),
+			float.Parse(parts[3]));
+	}
+}
