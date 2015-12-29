c2bba05c39fcbabcd3c6aee76c5bdff80cf1441a
denis <dovganichd@digicode.net>
2015-12-29 10:07:05 +0100

    Layout switcher

---
+++ Assets/Scripts/LayoutSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayoutSwitcher : MonoBehaviour 
+{
+	[SerializeField] float limitRatio = 1.4f;
+
+	[SerializeField] bool onUpdate = true;
+	bool wideScreen = false;
+	float aspectRatio = 0;
+
+	void Start()
+	{
+		if (!onUpdate)
+		{
+			CheckAspectRatio();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (onUpdate)
+		{
+			CheckAspectRatio();
+		}
+	}
+
+	void CheckAspectRatio()
+	{
+		float currentAspectRatio = (float)Screen.width / (float)Screen.height;
+
+		if (currentAspectRatio != aspectRatio)
+		{
+			aspectRatio = currentAspectRatio;
+
+			wideScreen = aspectRatio > limitRatio ? true : false;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(aspectRatio.ToString());
+		GUILayout.Label("Wide screen " + wideScreen.ToString());
+	}
+}
